951d32db935ddc9e4f7487668c8c7d9049b5a561
Aristocat <151950243+mpcgt@users.noreply.github.com>
2023-12-13 14:39:00 +0100

    Modifications des fichiers

---
+++ app/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <Page>
+    <ActionBar>
+      <Label text="Crowdly" class="font-bold text-lg" />
+    </ActionBar>
+
+    <GridLayout>
+      <Label
+        class="text-xl align-middle text-center text-gray-500"
+        :text="message"
+        @tap="logMessage"
+      />
+    </GridLayout>
+  </Page>
+</template>
+
+<div v-html="
+<p>bienvenue"</p></div>
+
+<style>
+/* .info {
+    font-size: 20;
+  } */
+</style>
